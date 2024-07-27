47c875f97bfd9cf94547ffcebd6b1a811d4f42f2
Giovanni <ggambier72@gmail.com>
2024-07-27 11:12:55 +0200

    Ajout d'un message dans Panier pour indiquer qui est vide

--- components/CartPage.js
+++ components/CartPage.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useCart } from "./cart/CartContext";
+import { FaShoppingCart } from "react-icons/fa"; // Importer l'icône de panier
 import styles from "../styles/CartPage.module.css";
 
 const CartPage = () => {
@@ -17,45 +18,54 @@ const CartPage = () => {
 
   return (
     <div className={styles.cartContainer}>
-      <h2>Mon Panier</h2>
-      <ul>
-        {cart.map((item) => (
-          <li key={item.id}>
-            <img src={item.image} alt={item.title} />
-            <div className={styles.productDetails}>
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-              <div className={styles.priceQuantity}>
-                <span>Prix: {item.price}</span>
-                <div className={styles.quantityButtons}>
+      {cart.length === 0 ? (
+        <div className={styles.emptyCartMessage}>
+          <FaShoppingCart size={50} />
+          <p>Votre panier est vide.</p>
+        </div>
+      ) : (
+        <>
+          <h2>Mon Panier</h2>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id}>
+                <img src={item.image} alt={item.title} />
+                <div className={styles.productDetails}>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                  <div className={styles.priceQuantity}>
+                    <span>Prix: {item.price}</span>
+                    <div className={styles.quantityButtons}>
+                      <button
+                        onClick={() =>
+                          handleQuantityChange(item.id, item.quantity - 1)
+                        }
+                      >
+                        -
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button
+                        onClick={() =>
+                          handleQuantityChange(item.id, item.quantity + 1)
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
                   <button
-                    onClick={() =>
-                      handleQuantityChange(item.id, item.quantity - 1)
-                    }
+                    className={styles.removeProduct}
+                    onClick={() => handleQuantityChange(item.id, 0)}
                   >
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button
-                    onClick={() =>
-                      handleQuantityChange(item.id, item.quantity + 1)
-                    }
-                  >
-                    +
+                    Supprimer tout le produit
                   </button>
                 </div>
-              </div>
-              <button
-                className={styles.removeProduct}
-                onClick={() => handleQuantityChange(item.id, 0)}
-              >
-                Supprimer tout le produit
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <h3 className={styles.total}>Total: {getTotal()} €</h3>
+              </li>
+            ))}
+          </ul>
+          <h3 className={styles.total}>Total: {getTotal()} €</h3>
+        </>
+      )}
     </div>
   );
 };
