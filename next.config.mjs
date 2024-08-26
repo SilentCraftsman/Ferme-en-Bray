8b5c8e46dc57b6e5b4693578bfda4a93ced4c446
Giovanni <ggambier72@gmail.com>
2024-08-26 12:40:33 +0200

    Vercel test

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@ const nextConfig = {
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASS: process.env.EMAIL_PASS,
   },
+  output: "export", // Pour l'exportation statique
+  // Assure-toi que le paramètre output est correctement configuré
 };
 
 export default nextConfig;
