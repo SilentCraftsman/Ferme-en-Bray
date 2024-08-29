3a0ea6d2c1367c28475dc48a790991e80e2cda85
Giovanni <ggambier72@gmail.com>
2024-08-29 22:04:54 +0200

    test CORS 32 error 405 fetch pm2

--- api/stripe/create-checkout-session.js
+++ api/stripe/create-checkout-session.js
@@ -1,39 +1,42 @@
+// /api/stripe/create-checkout-session.js
+
+import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const { items } = req.body;
-
-      // Créez une session de paiement Stripe
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: items.map((item) => ({
-          price_data: {
-            currency: "eur",
-            product_data: {
-              name: item.title,
-            },
-            unit_amount: Math.round(item.price * 100), // Convertir en centimes
-          },
-          quantity: item.quantity,
-        })),
-        mode: "payment",
-        success_url: `${process.env.NEXT_PUBLIC_URL}/success`,
-        cancel_url: `${process.env.NEXT_PUBLIC_URL}/cancel`,
-      });
-
-      res.status(200).json({ id: session.id });
-    } catch (err) {
-      console.error("Error creating checkout session:", err);
-      res.status(500).json({ error: err.message });
+export async function POST(request) {
+  try {
+    const { items } = await request.json();
+    if (!items || items.length === 0) {
+      return NextResponse.json({ error: "No items provided" }, { status: 400 });
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    // Créez une session de checkout avec Stripe
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: items.map((item) => ({
+        price_data: {
+          currency: "eur",
+          product_data: {
+            name: item.title,
+            // Ajouter d'autres détails de produit si nécessaire
+          },
+          unit_amount: item.price * 100, // en centimes
+        },
+        quantity: item.quantity,
+      })),
+      mode: "payment",
+      success_url: `${process.env.BASE_URL}/success`,
+      cancel_url: `${process.env.BASE_URL}/cancel`,
+    });
+
+    return NextResponse.json({ id: session.id });
+  } catch (error) {
+    console.error("Error creating checkout session:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
