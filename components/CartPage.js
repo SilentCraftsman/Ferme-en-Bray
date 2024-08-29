c3183baced4d8675f1920713f6d763ff9257f879
Giovanni <ggambier72@gmail.com>
2024-08-29 21:35:46 +0200

    test CORS 27 error 405 fetch pm2

--- components/CartPage.js
+++ components/CartPage.js
@@ -25,15 +25,12 @@ const CartPage = () => {
   const [customerEmail, setCustomerEmail] = useState("");
 
   useEffect(() => {
-    const loadStripe = async () => {
-      if (window.Stripe) {
-        setStripeLoaded(true);
-      } else {
-        console.error("Stripe.js has not loaded.");
-      }
-    };
-
-    loadStripe();
+    if (window.Stripe) {
+      setStripeLoaded(true);
+      console.log("Stripe.js is working !");
+    } else {
+      console.error("Stripe.js has not loaded.");
+    }
   }, []);
 
   const handleQuantityChange = (id, newQuantity) => {
@@ -41,6 +38,7 @@ const CartPage = () => {
       setError(`La quantité maximale pour cet article est ${MAX_QUANTITY}.`);
       newQuantity = MAX_QUANTITY;
     } else if (newQuantity < 0) {
+      setError("La quantité ne peut pas être négative.");
       newQuantity = 0; // Cela supprimera l'article si la quantité est 0
     } else {
       setError("");
@@ -106,20 +104,29 @@ const CartPage = () => {
     let isValid = true;
     let errorMessage = "";
 
-    if (!customerName) {
-      errorMessage = "Veuillez entrer un nom complet.";
-      isValid = false;
-    } else if (!customerAddress) {
-      errorMessage = "Veuillez entrer une adresse.";
-      isValid = false;
-    } else if (!customerEmail) {
-      errorMessage = "Veuillez entrer une adresse email.";
-      isValid = false;
-    } else if (!validateEmail(customerEmail)) {
-      errorMessage = "Veuillez entrer une adresse email valide.";
-      isValid = false;
-    } else if (!validateDateTime()) {
-      isValid = false;
+    switch (true) {
+      case !customerName:
+        errorMessage = "Veuillez entrer un nom complet.";
+        isValid = false;
+        break;
+      case !customerAddress:
+        errorMessage = "Veuillez entrer une adresse.";
+        isValid = false;
+        break;
+      case !customerEmail:
+        errorMessage = "Veuillez entrer une adresse email.";
+        isValid = false;
+        break;
+      case !validateEmail(customerEmail):
+        errorMessage = "Veuillez entrer une adresse email valide.";
+        isValid = false;
+        break;
+      case !validateDateTime():
+        isValid = false;
+        break;
+      default:
+        errorMessage = "";
+        break;
     }
 
     setError(errorMessage);
@@ -137,8 +144,10 @@ const CartPage = () => {
     }
 
     try {
+      console.log("Creating payment session...");
+
       const response = await fetch(
-        "/api/stripe/create-checkout-session", // Assurez-vous que le chemin est correct et que vous avez configuré le proxy si nécessaire
+        "https://ferme-en-bray.vercel.app/api/stripe/create-checkout-session",
         {
           method: "POST",
           headers: {
@@ -161,11 +170,18 @@ const CartPage = () => {
         }
       );
 
+      if (response.status === 405) {
+        throw new Error(
+          "Erreur HTTP 405: Méthode non autorisée. Vérifiez l'endpoint et les méthodes acceptées."
+        );
+      }
+
       if (!response.ok) {
         throw new Error(`Erreur HTTP! Status: ${response.status}`);
       }
 
       const data = await response.json();
+      console.log("Checkout session created:", data);
 
       const { id } = data;
       const stripe = window.Stripe(
@@ -182,135 +198,109 @@ const CartPage = () => {
         setError(error.message);
       }
     } catch (err) {
-      console.error("Error in createPayment:", err.message);
-      setError("Erreur lors de la création de la commande.");
+      console.error("Error in createPayment:", err);
+      setError(`Erreur lors de la création du paiement: ${err.message}`);
     }
   };
 
   return (
-    <div className={styles.cartContainer}>
+    <div className={styles.cartPage}>
       {cart.length === 0 ? (
         <div className={styles.emptyCartMessage}>
           <FaShoppingCart size={50} />
           <p>Votre panier est vide.</p>
         </div>
       ) : (
-        <>
-          <h2>Mon Panier</h2>
-          <ul>
-            {cart.map((item) => (
-              <li key={item.uniqueId}>
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className={styles.productImage}
+        <div>
+          <h1>Panier</h1>
+          {cart.map((item) => (
+            <div key={item.uniqueId} className={styles.cartItem}>
+              <img
+                src={item.image}
+                alt={item.title}
+                className={styles.productImage}
+              />
+              <h2>{getUpdatedTitle(item)}</h2>
+              <p>
+                Quantité:
+                <input
+                  type="number"
+                  min="0"
+                  max={MAX_QUANTITY}
+                  value={item.quantity}
+                  onChange={(e) =>
+                    handleQuantityChange(
+                      item.uniqueId,
+                      parseInt(e.target.value)
+                    )
+                  }
                 />
-                <div className={styles.productDetails}>
-                  <h3>{getUpdatedTitle(item)}</h3>
-                  <p>{item.description || "Description non disponible"}</p>
-                  <div className={styles.priceQuantity}>
-                    {item.selectedVariant && (
-                      <span>
-                        {item.selectedVariant.type} -{" "}
-                        {item.selectedVariant.weight}
-                      </span>
-                    )}
-                    <div className={styles.priceInfo}>
-                      <span>Prix unitaire: {getUnitPrice(item)} €</span>
-                      <span>
-                        Total pour cet article: {calculateTotalPrice(item)} €
-                      </span>
-                    </div>
-                  </div>
-                  <div className={styles.quantityButtons}>
-                    <button
-                      onClick={() =>
-                        handleQuantityChange(item.uniqueId, item.quantity - 1)
-                      }
-                    >
-                      -
-                    </button>
-                    <span>{item.quantity}</span>
-                    <button
-                      onClick={() =>
-                        handleQuantityChange(item.uniqueId, item.quantity + 1)
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
-                  <button
-                    className={styles.removeProduct}
-                    onClick={() => handleQuantityChange(item.uniqueId, 0)}
-                  >
-                    Supprimer tout le produit
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-          <h3 className={styles.total}>Total: {getTotal()} €</h3>
-          <div className={styles.datePickerContainer}>
-            <h3 className={styles.underlineTitle}>
-              Précisez le jour et l'heure de retrait :
-            </h3>
-            <select
-              className={styles.dateSelect}
-              value={pickupDay}
-              onChange={(e) => setPickupDay(e.target.value)}
-            >
-              <option value="vendredi">Vendredi</option>
-              <option value="samedi">Samedi</option>
-            </select>
-            <select
-              className={styles.dateSelect}
-              value={pickupTime}
-              onChange={(e) => setPickupTime(e.target.value)}
-            >
-              <option value="17:30">17:30</option>
-              <option value="18:00">18:00</option>
-              <option value="18:30">18:30</option>
-              <option value="19:00">19:00</option>
-              <option value="19:30">19:30</option>
-              <option value="20:00">20:00</option>
-            </select>
-            {dateError && (
-              <div className={styles.errorMessage}>{dateError}</div>
-            )}
+              </p>
+              <p>Prix unitaire: {getUnitPrice(item)}€</p>
+              <p>Total: {calculateTotalPrice(item)}€</p>
+              <button onClick={() => handleQuantityChange(item.uniqueId, 0)}>
+                Supprimer
+              </button>
+            </div>
+          ))}
+          <div className={styles.cartTotal}>
+            <h3>Total: {getTotal()}€</h3>
           </div>
-          <div className={styles.customerInfo}>
-            <h3>Renseignements nécessaires pour la commande :</h3>
-            <input
-              type="text"
-              placeholder="Nom complet"
-              value={customerName}
-              onChange={(e) => setCustomerName(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Adresse"
-              value={customerAddress}
-              onChange={(e) => setCustomerAddress(e.target.value)}
-            />
-            <input
-              type="email"
-              placeholder="Adresse email"
-              value={customerEmail}
-              onChange={(e) => setCustomerEmail(e.target.value)}
-            />
-            {error && (
-              <div
-                className={styles.errorMessage}
-                style={{ marginTop: "1rem" }}
+          <div className={styles.cartForm}>
+            <h3>Informations de Livraison</h3>
+            <label>
+              Nom complet:
+              <input
+                type="text"
+                value={customerName}
+                onChange={(e) => setCustomerName(e.target.value)}
+              />
+            </label>
+            <label>
+              Adresse:
+              <input
+                type="text"
+                value={customerAddress}
+                onChange={(e) => setCustomerAddress(e.target.value)}
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                value={customerEmail}
+                onChange={(e) => setCustomerEmail(e.target.value)}
+              />
+            </label>
+            <label>
+              Jour de retrait:
+              <select
+                value={pickupDay}
+                onChange={(e) => setPickupDay(e.target.value)}
               >
-                {error}
-              </div>
-            )}
+                <option value="vendredi">Vendredi</option>
+                <option value="samedi">Samedi</option>
+              </select>
+            </label>
+            <label>
+              Heure de retrait:
+              <select
+                value={pickupTime}
+                onChange={(e) => setPickupTime(e.target.value)}
+              >
+                <option value="17:30">17:30</option>
+                <option value="18:00">18:00</option>
+                <option value="18:30">18:30</option>
+                <option value="19:00">19:00</option>
+                <option value="19:30">19:30</option>
+                <option value="20:00">20:00</option>
+              </select>
+            </label>
+            {dateError && <div className={styles.error}>{dateError}</div>}
+            {error && <div className={styles.error}>{error}</div>}
+            <button onClick={createPayment}>Procéder au paiement</button>
           </div>
-          <button className={styles.paymentButton} onClick={createPayment}>
-            Procéder au paiement
-          </button>
-        </>
+        </div>
       )}
     </div>
   );
