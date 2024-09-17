8b3f316b4602a1372a5e814b23b93fe730eda9e1
Charles HL <61110764+Charles-HL@users.noreply.github.com>
2024-09-17 23:38:17 +0200

    Add script to build front+back for prod (#6)

--- build.sh
+++ build.sh
@@ -13,15 +13,53 @@ log_success() {
     echo -e "\033[0;32m$1\033[0m"
 }
 
-if ! cd "$SCRIPT_DIR"; then
-    log_error "Failed to change directory to $SCRIPT_DIR"
+if ! cd "$SCRIPT_DIR/frontend"; then
+    log_error "Failed to change directory"
     exit 1
 fi
-
-# Build the frontend
+log_info "Installing frontend dependencies..."
+if ! npm install; then
+    log_error "Failed to install frontend dependencies"
+    exit 1
+fi
 log_info "Building frontend..."
 if ! npm run build; then
     log_error "Failed to build frontend"
     exit 1
 fi
 
+if ! cd "$SCRIPT_DIR"; then
+    log_error "Failed to change directory to $SCRIPT_DIR"
+    exit 1
+fi
+
+# delete if exist backend/public and copy frontend build to backend/public
+if [ -d "backend/public" ]; then
+    log_info "Deleting existing backend/public directory..."
+    if ! rm -rf backend/public; then
+        log_error "Failed to delete backend/public directory"
+        exit 1
+    fi
+fi
+log_info "Creating backend/public directory..."
+if ! mkdir -p backend/public; then
+    log_error "Failed to create backend/public directory"
+    exit 1
+fi
+log_info "Copying frontend build to backend/public..."
+if ! cp -r frontend/out/* backend/public; then
+    log_error "Failed to copy frontend build to backend/public"
+    exit 1
+fi
+
+if ! cd "$SCRIPT_DIR/backend"; then
+    log_error "Failed to change directory to $SCRIPT_DIR/backend"
+    exit 1
+fi
+log_info "Installing backend dependencies..."
+if ! npm install; then
+    log_error "Failed to install backend dependencies"
+    exit 1
+fi
+
+log_success "Build completed successfully!"
